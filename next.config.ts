64ba0646940ead9da7c969e4d4e59883fea7e563
Jenaide <jenaidesibolie@gmail.com>
2024-12-08 20:47:32 +0200

    made changes to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'next_portfolio_2025' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
